c6cab1d9f7de246228f3a4bd8712a46f8ae4be8f
Silvija Gvazdaityte <sgvazdaityte@gmail.com>
2020-11-22 17:11:45 +0200

    Added CV and updated Index page

--- js/data_p4tterns.js
+++ js/data_p4tterns.js
@@ -7,11 +7,11 @@ var data = {
     { src: "assets/img/P4tterns/image--002.jpg", to: "3" },
     { src: "assets/img/P4tterns/image--003.jpg", to: "4" },
     { src: "assets/img/P4tterns/image--004.jpg", to: "5" },
-    { src: "assets/img/P4tterns/image3.png", to: "6" },
+    { src: "assets/img/P4tterns/Picture2.png", to: "6" },
     { src: "assets/img/P4tterns/image--005.jpg", to: "7" },
     { src: "assets/img/P4tterns/image--006.jpg", to: "8" },
     { src: "assets/img/P4tterns/image--007.jpg", to: "9" },
     { src: "assets/img/P4tterns/image--008.jpg", to: "10" },
-    { src: "assets/img/P4tterns/image4.png", to: "11" }
+    { src: "assets/img/P4tterns/Picture4.png", to: "11" }
   ]
 };
---
+++ js/projects.js
@@ -0,0 +1,84 @@
+var data = {
+  projects : [
+    {
+      title: "Monkey Mind",
+      description: "2D platforminis žaidimas apie šimpanzės nuotykius juodai-baltose džiunglėse.",
+      src: "assets/img/MonkeyMind/Picture2.png",
+      alt: "Monkey Mind žaidimo ekranvaizdis",
+      url: "monkeymind.html",
+      order: "order-lg-first",
+      textDir: "text-lg-right",
+      dir: 'mr-0'
+    },
+    {
+      title: "OFIN.lt reklama",
+      description: "2D reklaminė animacija apie elektroninę biuro prekių parduotuvę.",
+      src: "assets/img/OF IN/Picture8.png",
+      alt: "Kadras iš animacijos OFIN.lt",
+      url: "ofinlt.html",
+      order: "",
+      textDir: "text-lg-left",
+      dir: 'ml-0'
+    },
+    {
+      title: "Travel Diary",
+      description: "Android programėlė, skirta redaguoti paveiksliukų lokacijos ir laiko metaduomenis.",
+      src: "assets/img/Travel Diary/Capture.png",
+      alt: "Travel Diary programėlės ekranvaizdis",
+      url: "traveldiary.html",
+      order: "order-lg-first",
+      textDir: "text-lg-right",
+      dir: 'mr-0'
+    },
+    {
+      title: "Las Aventuras del Monito",
+      description: "2D platforminis žaidimas. Bakalaurinio darbo projektas.",
+      src: "assets/img/Las Aventuras del Monito/Level5_3.jpg",
+      alt: "Las Aventuras del Monito žaidimo ekranvaizdis",
+      url: "lasaventurasdelmonito.html",
+      order: "",
+      textDir: "text-lg-left",
+      dir: 'ml-0'
+    },
+    {
+      title: "The Beast",
+      description: "2D animacija. Apie naktį, šešėlius, gyvūnų galvų trofėjus ir... pabaisą.",
+      src: "assets/img/The Beast/Picture2.png",
+      alt: "Kadras iš animacijos The Beast",
+      url: "thebeast.html",
+      order: "order-lg-first",
+      textDir: "text-lg-right",
+      dir: 'mr-0'
+    },
+    {
+      title: "Monkey Mind",
+      description: "Internetinė vaizdo žaidimų svetainė.",
+      src: "assets/img/Monkey Mind Website/Picture6.png",
+      alt: "",
+      url: "monkeymindwebsite.html",
+      order: "",
+      textDir: "text-lg-left",
+      dir: 'ml-0'
+    },
+    {
+      title: "UNBOX -Your Past-",
+      description: "Mobiliosios programėlės grafinio dizaino projektas.",
+      src: "assets/img/UNBOX Your Past/Mockup.png",
+      alt: "UNBOX Your Past mockup",
+      url: "unboxyourpast.html",
+      order: "order-lg-first",
+      textDir: "text-lg-right",
+      dir: 'mr-0'
+    },
+    {
+      title: "P4tterns",
+      description: "Žaidimas Android įrenginiams.",
+      src: "assets/img/P4tterns/pattern.jpg",
+      alt: "P4tterns žaidimo ekranvaizdis",
+      url: "p4tterns.html",
+      order: "",
+      textDir: "text-lg-left",
+      dir: 'ml-0'
+    }
+  ]
+};
